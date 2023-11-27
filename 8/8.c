0963c2c97dda474e3f70a087b354c313e3d19caf
usri rshid <usri.rachid@gmail.com>
2023-11-27 23:47:17 +0100

    modify

--- 8/8.c
+++ 8/8.c
@@ -1,69 +1,77 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
-/* 
-this first game using the basic of c language
+/*
+this first game using the basics of the C language
+*/
 
- */
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int secretNumber, playerNumber, level = 0, tried = 1, min = 1, max = 100, flag = 0;
-  	srand(time(0));
-  	secretNumber = rand() % max + min;
-  	
-  	
-	printf("hello this game about guessing winner number between 1 and 100 \n\n");
-	printf("let's  start the game !!!\n");
-	printf("what level you want to play : \n");
-	printf("level 1 : between 1 and 100\n");
-	printf("level 2 : between 1 and 1000\n");
-	printf("level 3 : between 1 and 10000\n");
-	scanf("%d",&level);
-	for ( ; flag == 1; flag += 0 )
+	srand(time(0));
+	secretNumber = rand() % max + min;
+
+	printf("Hello! This game is about guessing a winning number between 1 and 100.\n\n");
+	printf("Let's start the game!!!\n");
+	printf("What level do you want to play?\n");
+	printf("Level 1: between 1 and 100\n");
+	printf("Level 2: between 1 and 1000\n");
+	printf("Level 3: between 1 and 10000\n");
+	scanf("%d", &level);
+
+	while (flag == 0)
 	{
-	switch (level)
-	{
-	
-		case 1:
+		if (level == 1)
+		{
 			max = 100;
 			flag = 1;
-			break;
-		case 2:
+		}
+		else if (level == 2)
+		{
 			max = 1000;
 			flag = 1;
-			break;
-		case 3:
+		}
+		else if (level == 3)
+		{
 			max = 10000;
 			flag = 1;
-			break;
-		default:
-			printf("\nwrong level\n");
-			scanf("%d",&level);
+		}
+		else
+		{
+			printf("\nWrong level. Please enter a valid level: ");
+			scanf("%d", &level);
+		}
 	}
-	}		
-		
-	do {
-	
-			printf("enter number between 1 and 100 : ");
-			scanf("%d", &playerNumber);
+
+	do
+	{
+		printf("Enter a number between 1 and %d: ", max);
+		scanf("%d", &playerNumber);
+
+		if (playerNumber < 1 || playerNumber > max)
+		{
+			printf("Invalid input. Please enter a number between 1 and %d.\n", max);
+		}
+		else
+		{
 			if (secretNumber < playerNumber)
 			{
-					printf("your number is greater\n");
-					tried += 1;		
+				printf("Your number is greater.\n");
+				tried += 1;
 			}
 			else if (secretNumber > playerNumber)
 			{
-					printf("your number is smaller\n");
-					tried += 1;		
+				printf("Your number is smaller.\n");
+				tried += 1;
 			}
 			else
 			{
-				printf("we have winner here !!!! you have found in %d tries only ! congrats!!",tried);
+				printf("We have a winner here!!! You found it in %d tries. Congratulations!!\n", tried);
 			}
 		}
-		while(secretNumber != playerNumber);
-  
-	printf("\nsucces !\n");
+	} while (secretNumber != playerNumber);
+
+	printf("\nSuccess!\n");
 	return 0;
 }
